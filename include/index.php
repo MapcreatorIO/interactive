952bc373a9609a53484ecff1f9ea748877105e82
shodan <mega@ioexception.at>
2016-06-03 08:51:12 +0200

    Import from old repo

---
+++ include/index.php
@@ -0,0 +1,46 @@
+<?php
+
+if (!isset($_GET['path'])) {
+	echo "No path given for the map!"; die;
+} else {
+	$path = htmlentities($_GET['path']);
+}
+
+if(!isset($_GET['id'])) {
+	$id = "m4n-" . str_shuffle("withlovefrommaps4news");
+} else {
+	$id = htmlentities($_GET['id']);
+}
+
+$env = isset($_GET['env']) ? htmlentities($_GET['env']) : "online";
+
+if(isset($_GET['style'])) {
+	$style = htmlentities($_GET['style']);
+}
+
+?>
+<html>
+	<head>
+		<style type="text/css">
+			html, body {
+				padding: 0; margin: 0; height: 100%; width: 100%;
+			}
+		</style>
+		<?php if(isset($style)): ?>
+			<link href="<?=$style;?>" type="text/css" rel="stylesheet" id="m4n-style-custom"/>
+		<?php endif; ?>
+	</head>
+	<body>
+		<div>
+			<div id="<?=$id;?>">
+				<script src="https://<?=$env;?>.maps4news.com/ia/2.0/m4n.js" type="text/javascript"></script>
+				<script type="text/javascript">
+					var map = new M4nInteractive({
+							"path": "<?=$path;?>",
+							"environment": "<?=$env;?>"
+						}, document.getElementById("<?=$id;?>"));
+				</script>
+			</div>
+		</div>
+	</body>
+</html>
