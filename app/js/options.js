f049628c5b69b468b02c11794bbb765cbff386c5
Kevin Richter <me@kevinrichter.nl>
2016-07-28 17:00:28 +0200

    Version bump

--- app/js/options.js
+++ app/js/options.js
@@ -17,7 +17,7 @@
 	main.homeButton = typeof options.homeButton !== 'undefined' ? options.homeButton : true;
 	main.hotspotMargin = parseInt(options.hotspotMargin) || 10;
 
-	main.version = { map: '2.0', code: '2.0.5-1' };
+	main.version = { map: '2.0', code: '2.0.5-2' };
 
 	main.dev = (function() {
 		if(typeof options.debug !== 'undefined') {
