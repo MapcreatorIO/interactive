06e57028bb65fd3f92132ecbfdf669863a8bd79b
Sven <xoryo@users.noreply.github.com>
2017-02-22 14:31:48 +0100

    fixes paths for new env

--- include/index.php
+++ include/index.php
@@ -10,11 +10,9 @@ if (preg_match('/\W/', $valid) || strlen($valid) !== 32) {
 	header('HTTP/1.1 400 Bad Request'); die;
 }
 
-$path = "../../output/";
-if(!!preg_match("/^(online|beta)\\.maps4news\\.com$/i", $_SERVER['HTTP_HOST'])) {
-	$path = "/mnt/data/production/output/";
-} else if("/^staging\\.maps4news\\.com$/i") {
-	$path = "/mnt/data/staging/output/";
+$path = "/mnt/data/output/";
+if("/^bleeding\\.maps4news\\.com$/i") {
+	$path = "/var/ace/output/";
 }
 
 $data = file_get_contents($path . trim($id, '/') . '/map.json');
